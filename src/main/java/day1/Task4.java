52e7ad8fc93ef27dcbcbe8cf3d3cd24a3467cdc2
Adsky Code <adskycode@gmail.com>
2020-10-25 17:28:44 +0300

    Add day1 solution

--- src/main/java/day1/Task4.java
+++ src/main/java/day1/Task4.java
@@ -2,6 +2,11 @@ package day1;
 
 public class Task4 {
     public static void main(String[] args) {
-
+        int year = 1980;
+        int finalYear = 2020;
+        while (year <= finalYear) {
+            System.out.println("Олимпиада " + year + " года");
+            year += 4;
+        }
     }
 }
